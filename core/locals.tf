a25ce45669bf53392b9eae98ffae8f4a48ab5abe
Robert Jones <robert@p1technologies.com>
2025-12-04 10:15:00 -0800

    fix clustering_node_ip selection

--- core/locals.tf
+++ core/locals.tf
@@ -27,8 +27,8 @@ locals {
   permanent_disk_count             = var.block_volume_count == null ? max(3, var.node_instance_ocpus / 2) : var.block_volume_count
   cluster_node_ips                 = join(" ", [for i in module.qcluster.nodes : i.private_ip])
   swing_node_ips                   = var.create_swing_pool == true ? join(" ", [for i in module.swing_pool.nodes : i.private_ip]) : ""
-  clustering_node_id               = var.configure_on_swing_pool == "true" ? module.swing_pool.nodes[0].id : module.qcluster.nodes[0].id
-  clustering_node_ip               = var.configure_on_swing_pool == "true" ? module.swing_pool.nodes[0].private_ip : module.qcluster.nodes[0].private_ip
+  clustering_node_id               = var.configure_on_swing_pool == true ? module.swing_pool.nodes[0].id : module.qcluster.nodes[0].id
+  clustering_node_ip               = var.configure_on_swing_pool == true ? module.swing_pool.nodes[0].private_ip : module.qcluster.nodes[0].private_ip
   node_ips_and_fault_domains       = length(module.qcluster.nodes) >= 5 || length(module.qcluster.nodes) == 3 ? join(" ", [for i in module.qcluster.nodes : "${i.private_ip},${i.fault_domain}"]) : join(" ", [for i in module.qcluster.nodes : "${i.private_ip},None"])
   swing_node_ips_and_fault_domains = var.create_swing_pool == true ? length(module.swing_pool.nodes) >= 5 || length(module.swing_pool.nodes) == 3 ? join(" ", [for i in module.swing_pool.nodes : "${i.private_ip},${i.fault_domain}"]) : join(" ", [for i in module.swing_pool[0].nodes : "${i.private_ip},None"]) : ""
   object_storage_uris              = join(" ", var.persistent_storage.object_storage_uris)
